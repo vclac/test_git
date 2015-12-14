f915fbd348b1b52922182d4354a81b0a262e4360
cu_lac <cu_lac@lampart.com.vn>
2015-12-14 15:09:21 +0700

    re push

---
+++ index-2.php
@@ -0,0 +1,12 @@
+<?php
+    echo "index-2";
+    function index2(){
+        return '2';
+    }
+    function index2_1(){
+        return '2-1';
+    }
+    function index2_2(){
+        return '2-2';
+    }
+?>
